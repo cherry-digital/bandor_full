590e027252eb11474c0b3368199ae03a341b60be
cherry-digital <98630255+cherry-digital@users.noreply.github.com>
2022-02-11 20:10:36 +0100

    Add files via upload

---
+++ slideshow.js
@@ -0,0 +1,36 @@
+$('.slideshow-top').slick({
+    arrows: false,
+    dots: true,
+    autoplay: true,
+    autoplaySpeed: 3000
+});
+
+$('.slideshow-products').slick({
+    infinite: true,
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    arrows: false,
+    dots: true,
+    autoplay: true,
+    autoplaySpeed: 3000,
+    responsive: [
+        {
+            breakpoint: 1024,
+            settings: {
+                slidesToShow: 3,
+            }
+    },
+        {
+            breakpoint: 992,
+            settings: {
+                slidesToShow: 2,
+            }
+    },
+        {
+            breakpoint: 768,
+            settings: {
+                slidesToShow: 1,
+            }
+    }
+        ]
+});
